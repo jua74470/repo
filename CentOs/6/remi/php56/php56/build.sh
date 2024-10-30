5ccd1eaf5448c4b04391503191aa08c44902a611
jua74470 <164792441+jua74470@users.noreply.github.com>
2024-10-30 13:32:14 +0100

    Update build.sh

--- CentOs/6/remi/php56/php56/build.sh
+++ CentOs/6/remi/php56/php56/build.sh
@@ -14,14 +14,14 @@ mkdir -p $(rpm --eval '%{_srcrpmdir}')/
 mkdir -p $(rpm --eval '%{_buildrootdir}')/
 yum -y update
 yum -y install mock rpm-build scl-utils-build
-wget "https://github.com/jua74470/repo/raw/refs/heads/main/CentOs/6/remi/php56/php56/LICENSE" -O "$(rpm --eval '%{_sourcedir}')/LICENSE"
-wget "https://github.com/jua74470/repo/raw/refs/heads/main/CentOs/6/remi/php56/php56/Makefile" -O "$(rpm --eval '%{_sourcedir}')/Makefile"
-wget "https://github.com/jua74470/repo/raw/refs/heads/main/CentOs/6/remi/php56/php56/README" -O "$(rpm --eval '%{_sourcedir}')/README"
-#wget "https://raw.githubusercontent.com/jua74470/repo/2235666789cd90fa57b4648faadeff82583794d2/CentOs/6/remi/php56/php56/macros-build" -O "$(rpm --eval '%{_sourcedir}')/macros-build"
-wget "https://raw.githubusercontent.com/jua74470/repo/master/CentOs/6/remi/php56/php56/macros-build" -O "$(rpm --eval '%{_sourcedir}')/macros-build"
-wget "https://github.com/jua74470/repo/raw/refs/heads/main/CentOs/6/remi/php56/php56/php56.spec" -O "$(rpm --eval '%{_specdir}')/php56.spec"
+wget "https://github.com/jua74470/repo/raw/refs/heads/main/CentOs/6/remi/php56/php56/LICENSE" -qO "$(rpm --eval '%{_sourcedir}')/LICENSE"
+wget "https://github.com/jua74470/repo/raw/refs/heads/main/CentOs/6/remi/php56/php56/Makefile" -qO "$(rpm --eval '%{_sourcedir}')/Makefile"
+wget "https://github.com/jua74470/repo/raw/refs/heads/main/CentOs/6/remi/php56/php56/README" -qO "$(rpm --eval '%{_sourcedir}')/README"
+#wget "https://raw.githubusercontent.com/jua74470/repo/2235666789cd90fa57b4648faadeff82583794d2/CentOs/6/remi/php56/php56/macros-build" -qO "$(rpm --eval '%{_sourcedir}')/macros-build"
+wget "https://raw.githubusercontent.com/jua74470/repo/master/CentOs/6/remi/php56/php56/macros-build" -qO "$(rpm --eval '%{_sourcedir}')/macros-build"
+wget "https://github.com/jua74470/repo/raw/refs/heads/main/CentOs/6/remi/php56/php56/php56.spec" -qO "$(rpm --eval '%{_specdir}')/php56.spec"
 rpmbuild -bs "$(rpm --eval '%{_specdir}')/php56.spec"
-wget https://github.com/jua74470/repo/raw/refs/heads/main/CentOs/6/remi/php56/php56/centos-6-x86_64.cfg -O /etc/mock/centos-6-x86_64.cfg
+wget https://github.com/jua74470/repo/raw/refs/heads/main/CentOs/6/remi/php56/php56/centos-6-x86_64.cfg -qO /etc/mock/centos-6-x86_64.cfg
 mock -r centos-6-x86_64 --clean
 #mock -r centos-6-x86_64 --enable-network --shell
 mock -r centos-6-x86_64 --enable-network --init
