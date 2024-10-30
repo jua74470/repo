8c4f3cf745d00b4c41b8a2e39bf7be50a53aa928
jua74470 <164792441+jua74470@users.noreply.github.com>
2024-10-30 19:40:45 +0100

    Update build.sh

--- CentOs/6/remi/php56/php56/build.sh
+++ CentOs/6/remi/php56/php56/build.sh
@@ -21,8 +21,8 @@ yum -y install mock rpm-build scl-utils-build
 #wget "https://raw.githubusercontent.com/jua74470/repo/1d29f8bc2b69ed2c391aa5d13b42472f8bdd93d6/CentOs/6/remi/php56/php56/macros-build" -qO "$(rpm --eval '%{_sourcedir}')/macros-build"
 #wget "https://raw.githubusercontent.com/jua74470/repo/master/CentOs/6/remi/php56/php56/macros-build" -qO "$(rpm --eval '%{_sourcedir}')/macros-build"
 #wget "https://git.remirepo.net/cgit/rpms/scl-php56/php56.git/plain/macros-build" -qO "$(rpm --eval '%{_sourcedir}')/macros-build"
-#wget "https://raw.githubusercontent.com/jua74470/repo/$commit/CentOs/6/remi/php56/php56/php56.spec" -qO "$(rpm --eval '%{_specdir}')/php56.spec"
-wget "https://git.remirepo.net/cgit/rpms/scl-php56/php56.git/plain/php56.spec" -qO "$(rpm --eval '%{_specdir}')/php56.spec"
+wget "https://raw.githubusercontent.com/jua74470/repo/$commit/CentOs/6/remi/php56/php56/php56.spec" -qO "$(rpm --eval '%{_specdir}')/php56.spec"
+#wget "https://git.remirepo.net/cgit/rpms/scl-php56/php56.git/plain/php56.spec" -qO "$(rpm --eval '%{_specdir}')/php56.spec"
 rpmbuild -bs "$(rpm --eval '%{_specdir}')/php56.spec"
 #wget https://raw.githubusercontent.com/jua74470/repo/fffde02235d13aa366060a85ecf8b6806240a8b7/CentOs/6/remi/php56/php56/centos-6-x86_64.cfg -qO /etc/mock/centos-6-x86_64.cfg
 #mock -r centos-6-x86_64 --clean
@@ -32,15 +32,15 @@ rm -rf /var/cache/mock/*
 #rm -rf /var/lib/mock/epel-6-x86_64/result/*
 #mock -r centos-6-x86_64 --enable-network --shell --no-clean
 #mock -r centos-6-x86_64 --enable-network --init --no-clean >/var/lib/mock/
-mock -r fedora-39-x86_64 --enable-network --no-clean --rebuild $(rpm --eval '%{_srcrpmdir}')/php56-5.6-40$(rpm --eval '%{dist}').src.rpm/root/
+#mock -r fedora-39-x86_64 --enable-network --no-clean --rebuild $(rpm --eval '%{_srcrpmdir}')/php56-5.6-40$(rpm --eval '%{dist}').src.rpm/root/
 mock -r centos-6-x86_64 --enable-network --init --no-clean
 #mock -r fedora-39-x86_64 --enable-network --init --no-clean
 #mock -r fedora-39-x86_64 --enable-network --no-clean --install scl-utils-build
 mock -r centos-6-x86_64 --enable-network --no-clean --install scl-utils-build
 #mock -r fedora-39-x86_64 --enable-network --no-clean --rebuild $(rpm --eval '%{_srcrpmdir}')/php56-5.6-40$(rpm --eval '%{dist}').src.rpm
-mock -r fedora-39-x86_64 --enable-network --no-clean --rebuild $(rpm --eval '%{_srcrpmdir}')/php56-5.6-40$(rpm --eval '%{dist}').src.rpm
+mock -r centos-6-x86_64 --enable-network --no-clean --rebuild $(rpm --eval '%{_srcrpmdir}')/php56-5.6-40$(rpm --eval '%{dist}').src.rpm
 ls /var/lib/mock/
-mock -r fedora-39-x86_64 --enable-network --no-clean --rebuild $(rpm --eval '%{_srcrpmdir}')/php56-5.6-40$(rpm --eval '%{dist}').src.rpm/result
+#mock -r fedora-39-x86_64 --enable-network --no-clean --rebuild $(rpm --eval '%{_srcrpmdir}')/php56-5.6-40$(rpm --eval '%{dist}').src.rpm/result
 exit
 cd /var/lib/mock/epel-6-x86_64/root/
 rpm2cpio /var/lib/mock/epel-6-x86_64/result/php56-5.6-40.el6.x86_64.rpm | cpio -idmv
