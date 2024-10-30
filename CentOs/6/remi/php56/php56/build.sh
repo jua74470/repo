027f5c1703d803dbcaae0c9ef2b409c0bcf672e0
jua74470 <164792441+jua74470@users.noreply.github.com>
2024-10-30 12:44:17 +0100

    Create build.sh

---
+++ CentOs/6/remi/php56/php56/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+mkdir -p $(rpm --eval '%{_topdir}')/
+mkdir -p $(rpm --eval '%{_builddir}')/
+mkdir -p $(rpm --eval '%{_rpmdir}')/
+mkdir -p $(rpm --eval '%{_rpmdir}')/x86_64
+mkdir -p $(rpm --eval '%{_rpmdir}')/i386
+mkdir -p $(rpm --eval '%{_rpmdir}')/i486
+mkdir -p $(rpm --eval '%{_rpmdir}')/i586
+mkdir -p $(rpm --eval '%{_rpmdir}')/i686
+mkdir -p $(rpm --eval '%{_rpmdir}')/noarch
+mkdir -p $(rpm --eval '%{_sourcedir}')/
+mkdir -p $(rpm --eval '%{_specdir}')/
+mkdir -p $(rpm --eval '%{_srcrpmdir}')/
+mkdir -p $(rpm --eval '%{_buildrootdir}')/
+yum -y update
+yum -y install mock rpm-build
+wget "https://github.com/jua74470/repo/raw/refs/heads/main/CentOs/6/remi/php56/php56/LICENSE" -O "$(rpm --eval '%{_sourcedir}')/LICENSE"
+wget "https://github.com/jua74470/repo/raw/refs/heads/main/CentOs/6/remi/php56/php56/Makefile" -O "$(rpm --eval '%{_sourcedir}')/Makefile"
+wget "https://github.com/jua74470/repo/raw/refs/heads/main/CentOs/6/remi/php56/php56/README" -O "$(rpm --eval '%{_sourcedir}')/README"
+wget "https://raw.githubusercontent.com/jua74470/repo/2235666789cd90fa57b4648faadeff82583794d2/CentOs/6/remi/php56/php56/macros-build" -O "$(rpm --eval '%{_sourcedir}')/macros-build"
+wget "https://github.com/jua74470/repo/raw/refs/heads/main/CentOs/6/remi/php56/php56/php56.spec" -O "$(rpm --eval '%{_specdir}')/php56.spec"
+rpmbuild -bs '%{_specdir}')/php56.spec"
